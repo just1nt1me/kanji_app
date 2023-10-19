d84d7150ac43c7a653be5579aedc07f81f7002f6
Justin Carville <jccarville@gmail.com>
2023-10-20 00:35:59 +0900

    kanji test proceeds to next level, updates h2 level tag. still bug with second to last question feedback not displaying before level up

--- level_test/views.py
+++ level_test/views.py
@@ -25,28 +25,34 @@ def start_test(request):
     request.session['score'] = 0
     request.session['current_level_index'] = 0
 
+    request.session.modified = True
+
     return redirect('kanji_test')
 
 def kanji_test(request):
     # Ensure the types of session variables are as expected.
-    current_index = int(request.session.get('current_index', 0))
+    current_index = int(request.session.get('current_index'))
     kanji_deck = request.session.get('kanji_deck', [])
-    current_level_index = int(request.session.get('current_level_index', 0))
+    current_level_index = int(request.session.get('current_level_index'))
     levels = ["n5", "n4", "n3", "n2", "n1"]
 
-    if current_index >= 10:  # If 10 questions have been answered
+    print("current index is", current_index)
+    print("current level index is", current_level_index)
+    if current_index == 9:  # If 10 questions have been answered (starts at 0)
         if int(request.session['score']) < 5:  # If score is less than 5
             return render(request, 'test_failed.html', {'score': request.session.get('score')})
 
         # Check if there are more levels left
         if current_level_index < len(levels) - 1:
-            current_level_index += 1
-            request.session['current_level_index'] = current_level_index
+            request.session['current_level_index'] += 1
+            print("after adding one, the current level index is", current_level_index)
             kanji_for_level = list(Kanji.objects.filter(tags__level=levels[current_level_index]).values_list('id', flat=True))
             request.session['kanji_deck'] = random.sample(kanji_for_level, 10)
             request.session['current_index'] = 0
-            current_index = 0
+            request.session['score'] = 0
             kanji_deck = request.session.get('kanji_deck')
+            request.session.modified = True
+            return redirect('kanji_test')
         else:
             # All questions have been answered and all levels cleared
             return render(request, 'test_complete.html', {'score': request.session.get('score')})
@@ -56,6 +62,7 @@ def kanji_test(request):
     if request.method == 'POST':
         user_answer = request.POST.get('answer')
         similarity = spacy_similarity(user_answer, current_kanji.meaning)
+        prev_kanji = current_kanji.expression
         correct_answer = current_kanji.meaning
 
         if similarity >= 0.8:
@@ -74,14 +81,19 @@ def kanji_test(request):
         request.session['current_index'] += 1
         current_kanji = Kanji.objects.get(id=kanji_deck[int(request.session['current_index'])])  # Fetch the next kanji
 
+        request.session.modified = True
+
         # Check if the request is AJAX
         if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
             # Return result as JSON for AJAX
             return JsonResponse({
                 'is_correct': is_correct,
+                'prev_kanji': prev_kanji, # display prev_kanji in correct/incorrect feedback
                 'correct_answer': correct_answer,
                 'score': request.session['score'],
-                'kanji': current_kanji.expression  # Add the next kanji to the JSON response
+                'index': request.session['current_index'],
+                'kanji': current_kanji.expression,
+                'level': levels[current_level_index]
             })
 
     # For GET requests and non-AJAX POST requests
