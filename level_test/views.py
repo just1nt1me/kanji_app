ec9452f211c76c8c09d38f1affaa305261331116
Justin Carville <jccarville@gmail.com>
2023-10-21 09:25:35 +0900

    trying to fix level up bug

--- level_test/views.py
+++ level_test/views.py
@@ -36,27 +36,7 @@ def kanji_test(request):
     current_level_index = int(request.session.get('current_level_index'))
     levels = ["n5", "n4", "n3", "n2", "n1"]
 
-    print("current index is", current_index)
-    print("current level index is", current_level_index)
-    if current_index == 9:  # If 10 questions have been answered (starts at 0)
-        if int(request.session['score']) < 5:  # If score is less than 5
-            return render(request, 'test_failed.html', {'score': request.session.get('score')})
-
-        # Check if there are more levels left
-        if current_level_index < len(levels) - 1:
-            request.session['current_level_index'] += 1
-            print("after adding one, the current level index is", current_level_index)
-            kanji_for_level = list(Kanji.objects.filter(tags__level=levels[current_level_index]).values_list('id', flat=True))
-            request.session['kanji_deck'] = random.sample(kanji_for_level, 10)
-            request.session['current_index'] = 0
-            request.session['score'] = 0
-            kanji_deck = request.session.get('kanji_deck')
-            request.session.modified = True
-            return redirect('kanji_test')
-        else:
-            # All questions have been answered and all levels cleared
-            return render(request, 'test_complete.html', {'score': request.session.get('score')})
-
+    # Fetch current kanji
     current_kanji = Kanji.objects.get(id=kanji_deck[current_index])
 
     if request.method == 'POST':
@@ -78,17 +58,35 @@ def kanji_test(request):
             print(f"Incorrect. Answer: {user_answer}, Expected: {current_kanji.meaning}")
         print(f"Current Score: {request.session['score']}")
 
+        # Increment the current index
         request.session['current_index'] += 1
-        current_kanji = Kanji.objects.get(id=kanji_deck[int(request.session['current_index'])])  # Fetch the next kanji
+        current_index = request.session['current_index']
 
-        request.session.modified = True
+        # Check if all kanjis in the current deck have been processed
+        if current_index >= len(kanji_deck):
+            if int(request.session['score']) < 5:  # If score is less than 5
+                return render(request, 'test_failed.html', {'score': request.session.get('score')})
+
+            # Check if there are more levels left
+            if current_level_index < len(levels) - 1:
+                request.session['current_level_index'] += 1
+                kanji_for_level = list(Kanji.objects.filter(tags__level=levels[current_level_index]).values_list('id', flat=True))
+                request.session['kanji_deck'] = random.sample(kanji_for_level, 10)
+                request.session['current_index'] = 0
+                request.session['score'] = 0
+                request.session.modified = True
+                return redirect('kanji_test')
+            else:
+                # All questions have been answered and all levels cleared
+                return render(request, 'test_complete.html', {'score': request.session.get('score')})
+        else:
+            current_kanji = Kanji.objects.get(id=kanji_deck[current_index])  # Fetch the next kanji after changing index
 
         # Check if the request is AJAX
         if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-            # Return result as JSON for AJAX
             return JsonResponse({
                 'is_correct': is_correct,
-                'prev_kanji': prev_kanji, # display prev_kanji in correct/incorrect feedback
+                'prev_kanji': prev_kanji,
                 'correct_answer': correct_answer,
                 'score': request.session['score'],
                 'index': request.session['current_index'],
@@ -96,5 +94,5 @@ def kanji_test(request):
                 'level': levels[current_level_index]
             })
 
-    # For GET requests and non-AJAX POST requests
+    request.session.modified = True
     return render(request, 'level-test/kanji-test.html', {'kanji': current_kanji.expression, 'level': levels[current_level_index]})
