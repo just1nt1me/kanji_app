0dfa2313aa20929bf51794c0d57a7f049a1ccf0c
Justin Carville <jccarville@gmail.com>
2023-12-07 17:03:04 +0900

    remove JLPT model, integrate to Kanji model. Initialize function sets kanji progress for first sessions

--- study_decks/urls.py
+++ study_decks/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('register', views.register, name='register'),
     path('', views.study, name='study'),
-    path('study/<int:jlpt_level>', views.study_level, name='study-level'),
+    path('study/<str:jlpt_level>', views.study_level, name='study-level'),
 ]
